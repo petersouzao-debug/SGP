7a1d05eb29fc6c4992fe3e15dc31a45adc71b9f7
petersouzao-debug <peter.souzao@gmail.com>
2026-02-21 20:10:40 -0300

    Add files via upload
    SGP_CompararTurmas_DistribuicaoCache_v15_4

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// SGP - Service Worker v15.3 (Sorteador + Mapa + Grupos)
-const CACHE_NAME = 'sgp-v15.3';
+// SGP - Service Worker v15.4 (Sorteador + Mapa + Grupos)
+const CACHE_NAME = 'sgp-v15.4';
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(['./', './index.html', './SGP_Gestao_Final_v15.html']).catch(() => {})));
   self.skipWaiting();
