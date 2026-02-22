be5bcc38aab89d31d96c7035bb5529d4c595eb1b
petersouzao-debug <peter.souzao@gmail.com>
2026-02-22 15:30:17 -0300

    Add files via upload
    SGP_mapa_mesa_professor_below_grid_v15.9.5

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// SGP - Service Worker v15.9.2 (Ranking: rolagem + fullscreen)
-const CACHE_NAME = 'sgp-v15.9.2';
+// SGP - Service Worker v15.9.5 (Mapa: mesa professor)
+const CACHE_NAME = 'sgp-v15.9.5';
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(['./', './index.html', './SGP_Gestao_Final_v15.html']).catch(() => {})));
   self.skipWaiting();
